30a1cbe173b10c0872e57035a964529508b43bba
lingaiah-ctu <rao.lingaiah@gmail.com>
2018-05-16 14:24:29 -0700

    express is updated

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Course project  for Node.js 0 to WebApps
 
 Welcome To MEAN Stack Developement : NodeJS
+cls
--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-var aloha="Aloha Variables";
-console.log(aloha);
+// var aloha="Aloha Variables";
+// console.log(aloha);
+
+var express =require('express');
+var app=express();
+var port=5000;//process.env.PORT;
+app.listen(port, function(err)
+{
+console.log('The server is running on port '+port+'.....');
+});
+
